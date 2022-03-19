bed56557780c995aa5432548f0385b07b9bc03e7
nkululek0 <nkululekovuyo0103@gmail.com>
2022-03-19 14:57:03 +0200

    corrected import and export of router

--- app.js
+++ app.js
@@ -2,14 +2,17 @@ const express = require("express");
 const appRoutes = require("./routes/app-routes.js");
 const mongoose = require("mongoose");
 
+// middleware
+
+
 // creation of routes functionality
 const app = express();
 app.listen()
 
 // database connection
 const dbURI = 'mongodb+srv://nkululek0:mongodb@certification.bscdz.mongodb.net/node-auth';
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
-  .then((result) => app.listen(3000))
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => app.listen(3030))
   .catch((err) => console.log(err));
 
 // app routes
--- routes/app-routes.js
+++ routes/app-routes.js
@@ -1,8 +1,7 @@
-const express = require("express");
-const router = express.Router();
+const router = require("express").Router();
 const authController = require("../controllers/auth-controller.js");
 
 // GET requests
 router.get("/login", authController.getLogin);
 
-module.exoprts = { router };
+module.exports = router;
