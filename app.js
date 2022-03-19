cf074951b612324dec2d94b84e9566b6e5746096
nkululek0 <nkululekovuyo0103@gmail.com>
2022-03-19 16:07:01 +0200

    added json middle so application can parse json

--- app.js
+++ app.js
@@ -2,12 +2,11 @@ const express = require("express");
 const appRoutes = require("./routes/app-routes.js");
 const mongoose = require("mongoose");
 
-// middleware
-
-
 // creation of routes functionality
 const app = express();
-app.listen();
+
+// middleware
+app.use(express.json());
 
 // database connection
 const dbURI = 'mongodb+srv://nkululek0:mongodb@certification.bscdz.mongodb.net/node-auth';
