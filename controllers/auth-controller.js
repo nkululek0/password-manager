bff5002ea79acfd8a5dc3fe3381d5d3bb102d5e7
nkululek0 <nkululekovuyo0103@gmail.com>
2022-03-19 17:36:14 +0200

    created sign up functionality and error handling

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -10,10 +10,39 @@ module.exports.getSignUp = function(req, res) {
 }
 
 // POST requests
-module.exports.postLogin = function(req, res) {
-    res.send("Login data sent");
+module.exports.postLogin = async function(req, res) {
+    res.send("login data sent");
 }
 
-module.exports.postSignUp = function(req, res) {
-    res.send("Sign up data sent");
+module.exports.postSignUp = async function(req, res) {
+    const { username, email, password } = req.body;
+
+    try {
+        const user = await User.create({ username, email, password });
+        console.log(`user ${user.username} successfully created`);
+        res.json({ user });
+    } catch(err) {
+        const errorMessages = signUpErrors(err);
+        res.json({ errorMessages });
+    }
+}
+
+// function that deals with errors
+function signUpErrors(err) {
+    let errorMessages = { username: "", email: "", password: "" };
+
+    // duplicate email error
+    if(err.code === 11000) {
+        errorMessages.email = "This email is already registered"
+        console.log(errorMessages);
+        return errorMessages;
+    }
+
+    // validation errors
+    if(err.message.includes("User validation failed")) {
+        Object.values(err.errors).forEach(function({ properties }) {
+            errorMessages[properties.path] = properties.message;
+        });
+        return errorMessages;
+    }
 }
