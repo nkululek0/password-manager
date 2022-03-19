d722896fa38350dcf95ba6fad8e9afeac45a7b2f
nkululek0 <nkululekovuyo0103@gmail.com>
2022-03-19 14:30:42 +0200

    updated email validation on schema

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -1,6 +1,6 @@
 const User = require("../models/user.js");
 
 // GET requests
-module.exports.getLogin = (req, res) => {
+module.exports.getLogin = function (req, res) {
     res.send("login page");
 }
