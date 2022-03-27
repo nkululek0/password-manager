a637059445f7af3162fb732d949f1d40b576ae39
nkululek0 <nkululekovuyo0103@gmail.com>
2022-03-27 14:15:34 +0200

    moved putPasswordAccount to nav-controller.js

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -42,32 +42,6 @@ module.exports.postLogin = async function(req, res) {
     }
 }
 
-// submits content when creating a password account
-module.exports.patchPasswordAccount = async function(req, res) {
-    const { accountName, accountUsername, accountPassword } = req.body;
-
-    try {
-        const user = await User.findById(req.params.id);
-
-        // user does not exist error
-        if(user === null) {
-            return res.status(404).json({ error: "user not found"});
-        }
-        
-        // password account already exists error
-        if(findValue(user.accounts, accountName)) {
-            return res.status(400).json({ error: "account already exists, update instead?" });
-        } else {
-            user.accounts.push({ accountName, accountUsername, accountPassword });
-            await user.save();
-        }
-        console.log(`created account ${ accountName } for user ${ user.email }`);
-        res.json({ user });
-    } catch(err) {
-        res.json({ error: err.message });
-    }
-}
-
 
 // function that deals with errors for postSignUp
 function signUpErrors(err) {
@@ -110,9 +84,4 @@ const maxAge = 600;
 
 function createToken(payload) {
     return jwt.sign({ payload }, process.env.SECRECT_KEY, { expiresIn: maxAge });
-}
-
-// find a value and return true if it exists
-function findValue(arr, value) {
-    return arr.find(function(prop) { return prop.accountName === value });
 }
