332ac051c4b7f94e34db6bfaba3f7fc715574c7b
nkululek0 <nkululekovuyo0103@gmail.com>
2022-04-15 16:19:48 +0200

    created authorise user functionality using jwt cookie

---
+++ middleware/authorise.js
@@ -0,0 +1,24 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/user.js");
+
+function authorise(req, res, next) {
+    const loginCookie = req.cookies.login;
+
+    if(loginCookie) {
+        jwt.verify(loginCookie, process.env.SECRECT_KEY, async function(err, decodedToken) {
+            if(err) {
+                console.log(err.message);
+                res.redirect("/api/login");
+            } else {
+                console.log(decodedToken);
+                let user = await User.findById(decodedToken.id);
+                res.locals.user = user;
+                next();
+            }
+        });
+    } else {
+        res.redirect("/api/login");
+    }
+}
+
+module.exports = { authorise };
