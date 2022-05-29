fdcc3229149e7416e60dff18509754e424231ef1
nkululek0 <nkululekovuyo0103@gmail.com>
2022-05-29 16:36:13 +0200

    fixed bug that returns first user account when logging in even though user enter different credentials

--- middleware/authorise.js
+++ middleware/authorise.js
@@ -11,7 +11,7 @@ function authorise(req, res, next) {
                 res.redirect("/api/login");
             } else {
                 console.log(decodedToken);
-                let user = await User.findOne({ id: decodedToken.payload });
+                let user = await User.findById(decodedToken.payload);
                 res.locals.user = user;
                 next();
             }
