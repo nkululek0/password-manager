531f45a8fa2b9278ef576f86a7d0e3f3004e6705
nkululek0 <nkululekovuyo0103@gmail.com>
2022-05-31 17:43:25 +0200

    created controller for searching of password accounts

---
+++ controllers/search-account-controller.js
@@ -0,0 +1,27 @@
+const User = require("../models/user.js");
+
+module.exports.searchAccount = async function(req, res) {
+    let accountName = req.body.accountName.trim();
+
+    try {
+        // find the user based on the email and return accounts
+        const userPasswordAccounts = await User.findOne({ email: req.params.email }, { _id: 0, accounts: 1 });
+
+        // error for when the user is not found
+        if(userPasswordAccounts === null) {
+            return res.status(404).json({ error: "user not found" });
+        }
+
+        // returns password accounts based on user search
+        const accounts = userPasswordAccounts.accounts.filter(function(item) {
+            let expr = new RegExp(`^${ accountName }.*`, "i");
+            if(expr.test(item.accountName)) {
+                return item;
+            }
+        });
+
+        res.json(accounts);
+    } catch(err) {
+        res.json({ error: err.message });
+    }
+}
