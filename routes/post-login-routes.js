845a0cba462af2efcc8b890323fd2ced322472c0
nkululek0 <nkululekovuyo0103@gmail.com>
2022-06-09 17:44:59 +0200

    changed deletePasswordAccount request method from get to patch

--- routes/post-login-routes.js
+++ routes/post-login-routes.js
@@ -4,9 +4,9 @@ const navController = require("../controllers/nav-controller.js");
 // PATCH requests
 router.patch("/create-password-account/users/:email", navController.createPasswordAccount);
 router.patch("/update-password-account/users/:email/:accountName", navController.updatePasswordAccount);
+router.patch("/delete-password-account/users/:email/:accountName", navController.deletePasswordAccount);
 
 // GET requests
-router.get("/delete-password-account/users/:email/:accountName", navController.deletePasswordAccount);
 router.get("/logout/users", navController.logoutUser);
 
 // DELETE requests
